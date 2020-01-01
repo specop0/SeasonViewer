1c59d5105a768a61139c205e0bcab56a1db34893
SpecOp0 <SpecOp0@yahoo.de>
2020-01-01 15:05:29 +0100

    group anime by hoster ... on client side

--- SeasonViewer/Data/HosterView.cs
+++ SeasonViewer/Data/HosterView.cs
@@ -41,5 +41,24 @@ namespace SeasonViewer.Data
         }
 
         public string Url => this.Model.Url;
+
+        public static string HosterTypeToImageUrl(HosterType type)
+        {
+            switch (type)
+            {
+                case HosterType.Amazon:
+                    return "icons/amazon.ico";
+                case HosterType.AnimeOnDemand:
+                    return "icons/anime-on-demand.ico";
+                case HosterType.Crunchyroll:
+                    return "icons/crunchyroll.ico";
+                case HosterType.Netflix:
+                    return "icons/netflix.ico";
+                case HosterType.Wakanim:
+                    return "icons/wakanim.ico";
+                default:
+                    return "";
+            }
+        }
     }
 }
