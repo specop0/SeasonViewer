4dcbafa975eda8f3e08f4fb2c874d3a9de0ad540
SpecOp0 <SpecOp0@yahoo.de>
2020-01-31 14:45:49 +0100

    print exception message for miner

--- SeleniumMiner/src/main/java/anime/seleniumminer/main/Main.java
+++ SeleniumMiner/src/main/java/anime/seleniumminer/main/Main.java
@@ -11,22 +11,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        int port = 22471;
-        String settingsFilename = "settings.json";
-        if (args.length > 0) {
-            settingsFilename = args[0];
+        try {
+
+            int port = 22471;
+            String settingsFilename = "settings.json";
+            if (args.length > 0) {
+                settingsFilename = args[0];
+            }
+
+            IWebDriver driver = ChromeDriverEx.GetNewWebDriver();
+            IPlugin[] plugins = Plugins.GetAllPlugins();
+            IMineController controller = new MineController(driver, plugins);
+
+            Routes.EstablishRoutes(port, controller);
+
+            System.out.println(String.format("Endpoint listening at: localhost:%d", port));
+
+            System.in.read();
+            spark.Spark.stop();
+            spark.Spark.awaitStop();
+        } catch (Throwable  ex) {
+            System.out.println(ex.getMessage());
+            System.out.println(ex);
         }
-
-        IWebDriver driver = ChromeDriverEx.GetNewWebDriver();
-        IPlugin[] plugins = Plugins.GetAllPlugins();
-        IMineController controller = new MineController(driver, plugins);
-
-        Routes.EstablishRoutes(port, controller);
-
-        System.out.println(String.format("Endpoint listening at: localhost:%d", port));
-
-        System.in.read();
-        spark.Spark.stop();
-        spark.Spark.awaitStop();
     }
 }
