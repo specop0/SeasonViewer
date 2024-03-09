c70b4137893d4f8557d2faa417e29091e862b11a
SpecOp0 <SpecOp0@yahoo.de>
2024-03-09 12:17:59 +0100

    chore: update .NET framework & dependencies

--- SeasonTests/Backend/Miner/SeleniumMinerTests.cs
+++ SeasonTests/Backend/Miner/SeleniumMinerTests.cs
@@ -28,13 +28,13 @@
             var input = this.GetResource("anime.json");
             var actualAnime = SeleniumMiner.ParseAnime(input);
 
-            Assert.IsNotNull(actualAnime);
-            Assert.AreEqual("Boku no Hero Academia 4th Season", actualAnime.Mal.Name);
-            Assert.AreEqual("38408", actualAnime.Mal.Id);
-            Assert.AreEqual(25, actualAnime.Mal.EpisodesCount);
-            Assert.AreEqual(813, actualAnime.Mal.Score);
-            Assert.AreEqual(742500, actualAnime.Mal.MemberCount);
-            Assert.AreEqual("https://cdn.myanimelist.net/images/anime/1315/102961.jpg", actualAnime.Mal.ImageUrl);
+            Assert.That(actualAnime, Is.Not.Null);
+            Assert.That(actualAnime.Mal.Name, Is.EqualTo("Boku no Hero Academia 4th Season"));
+            Assert.That(actualAnime.Mal.Id, Is.EqualTo("38408"));
+            Assert.That(actualAnime.Mal.EpisodesCount, Is.EqualTo(25));
+            Assert.That(actualAnime.Mal.Score, Is.EqualTo(813));
+            Assert.That(actualAnime.Mal.MemberCount, Is.EqualTo(742500));
+            Assert.That(actualAnime.Mal.ImageUrl, Is.EqualTo("https://cdn.myanimelist.net/images/anime/1315/102961.jpg"));
         }
 
         [Test]
@@ -43,31 +43,31 @@
             var input = this.GetResource("mal-season.json");
             var season = this.GetUniqueName();
             var actualAnimes = SeleniumMiner.ParseSeasonAnime(input, season);
-            CollectionAssert.IsNotEmpty(actualAnimes);
+            Assert.That(actualAnimes, Is.Not.Empty);
 
             var expectedAnimes = JsonSerializer.Deserialize<Anime[]>(this.GetResource("mal-season-expected.json"));
-            CollectionAssert.IsNotEmpty(expectedAnimes, "unit test implementation");
+            Assert.That(expectedAnimes, Is.Not.Empty, "unit test implementation");
             // System.IO.File.WriteAllText(@"mal-season-expected.json", JsonSerializer.Serialize(actualAnimes, new JsonSerializerOptions { WriteIndented = true, DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull }));
 
-            Assert.AreEqual(expectedAnimes.Length, actualAnimes.Length);
+            Assert.That(actualAnimes.Length, Is.EqualTo(expectedAnimes.Length));
 
             for (var i = 0; i < expectedAnimes.Length; i++)
             {
                 var expected = expectedAnimes[i];
                 var actual = actualAnimes[i];
 
-                Assert.AreEqual(expected.Mal.Id, actual.Mal.Id);
-                Assert.AreEqual(expected.Mal.Name, actual.Mal.Name);
-                Assert.AreEqual(expected.Mal.ImageUrl, actual.Mal.ImageUrl);
-                Assert.AreEqual(expected.Mal.MemberCount, actual.Mal.MemberCount);
-                Assert.AreEqual(expected.Mal.Score, actual.Mal.Score);
+                Assert.That(actual.Mal.Id, Is.EqualTo(expected.Mal.Id));
+                Assert.That(actual.Mal.Name, Is.EqualTo(expected.Mal.Name));
+                Assert.That(actual.Mal.ImageUrl, Is.EqualTo(expected.Mal.ImageUrl));
+                Assert.That(actual.Mal.MemberCount, Is.EqualTo(expected.Mal.MemberCount));
+                Assert.That(actual.Mal.Score, Is.EqualTo(expected.Mal.Score));
 
-                CollectionAssert.AreEqual(new[] { season }, actual.Seasons);
-                Assert.AreEqual(default(int), actual.Id);
+                Assert.That(actual.Seasons, Is.EqualTo(new[] { season }));
+                Assert.That(actual.Id, Is.EqualTo(default(int)));
 
-                CollectionAssert.IsEmpty(actual.Hoster);
+                Assert.That(actual.Hoster, Is.Empty);
 
-                Assert.IsNotNull(expected.Mal.ImageUrl, $"missing image for {expected.Mal.Name}");
+                Assert.That(expected.Mal.ImageUrl, Is.Not.Null, $"missing image for {expected.Mal.Name}");
             }
         }
 
@@ -78,16 +78,16 @@
             var searchResult = SeleniumMiner.ParseDuckDuckGoSearch(input);
 
             var expectedResults = JsonSerializer.Deserialize<DuckDuckGoSearchItem[]>(this.GetResource("duckduckgo-search-expected.json"));
-            CollectionAssert.IsNotEmpty(expectedResults, "unit test implementation");
-            Assert.AreEqual(expectedResults.Length, searchResult.Length);
+            Assert.That(expectedResults, Is.Not.Empty, "unit test implementation");
+            Assert.That(searchResult.Length, Is.EqualTo(expectedResults.Length));
 
             for (var i = 0; i < expectedResults.Length; i++)
             {
                 var expected = expectedResults[i];
                 var actual = searchResult[i];
 
-                Assert.AreEqual(expected.Name, actual.Name);
-                Assert.AreEqual(expected.Url, actual.Url);
+                Assert.That(actual.Name, Is.EqualTo(expected.Name));
+                Assert.That(actual.Url, Is.EqualTo(expected.Url));
             }
         }
 
@@ -97,25 +97,25 @@
             var input = this.GetResource("mal-list.json");
             var listEntries = SeleniumMiner.ParseMalList(input);
 
-            Assert.Greater(listEntries.Count(), 1000);
+            Assert.That(listEntries.Count(), Is.GreaterThan(1000));
 
             var expectedResults = JsonSerializer.Deserialize<MalListMineResult[]>(this.GetResource("mal-list-expected.json"));
-            CollectionAssert.IsNotEmpty(expectedResults, "unit test implementation");
-            Assert.AreEqual(expectedResults.Length, listEntries.Length);
+            Assert.That(expectedResults, Is.Not.Empty, "unit test implementation");
+            Assert.That(listEntries.Length, Is.EqualTo(expectedResults.Length));
 
             for (var i = 0; i < expectedResults.Length; i++)
             {
                 var expected = expectedResults[i];
                 var actual = listEntries[i];
 
-                Assert.AreEqual(expected.AnimeId, actual.AnimeId);
-                Assert.AreEqual(expected.Status, actual.Status);
+                Assert.That(actual.AnimeId, Is.EqualTo(expected.AnimeId));
+                Assert.That(actual.Status, Is.EqualTo(expected.Status));
             }
 
             var groupedByStatus = listEntries.GroupBy(x => x.Status).ToDictionary(x => x.Key, x => x.ToArray());
             var everyValidStatus = Enum.GetValues(typeof(ListStatus)).Cast<ListStatus>().Except(new[] { ListStatus.Unknown });
-            CollectionAssert.AreEquivalent(everyValidStatus, groupedByStatus.Keys);
-            Assert.IsTrue(groupedByStatus.All(x => x.Value.Any()));
+            Assert.That(groupedByStatus.Keys, Is.EquivalentTo(everyValidStatus));
+            Assert.That(groupedByStatus.All(x => x.Value.Any()), Is.True);
         }
 
         [Test, Ignore("Miner must be listening")]
@@ -130,7 +130,7 @@
             var configuration = configurationBuilder.AddInMemoryCollection(configurationDictionary).Build();
             var testee = new SeleniumMiner(new HttpClient(), configuration);
             var animes = testee.MineSeasonAnimeAsync(season).Result.Animes;
-            CollectionAssert.IsNotEmpty(animes);
+            Assert.That(animes, Is.Not.Empty);
             System.IO.File.WriteAllText(
                 "expected.json",
                 JsonSerializer.Serialize(animes, new JsonSerializerOptions { WriteIndented = true, DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull }));
